6b8f1795555284bfbdeea13ce9b75db57bdb0273
iker <iker.garay@ironchip.net>
2022-08-12 12:36:19 +0200

    fix package.swift error

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "LBFraudSDKiOS",
-            path: "./Source/LBFraudSDKiOS.xcframework"
+            path: "./Sources/LBFraudSDKiOS.xcframework"
         )
     ]
 )
