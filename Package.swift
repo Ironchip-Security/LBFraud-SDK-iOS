06ee7bdea7ade37eaf15d69d1fc41979b46cf80f
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-09-07 15:04:33 +0200

    change one .xcframework as lbfraud into two .xcframework.zip as commons and lbfraud

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LBFraudSDKiOS",
-            targets: ["LBFraudSDKiOS"]),
+            targets: ["IronchipCommonsIos", "LBFraudSDKiOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,8 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "LBFraudSDKiOS",
-            path: "./Sources/LBFraudSDKiOS.xcframework"
-        )
+                     name: "IronchipCommonsIos",
+                     url: "https://nexus6.kydom.net/repository/FrameworkA/IronchipCommonsIos/IronchipCommonsIos.xcframework.zip",
+                    checksum: "aa1455639d06e91a75f5754aadf9cb13dc6025aa1af2d53ecf844c2a575fea47"),
+        .binaryTarget(
+                     name: "LBFraudSDKiOS",
+                     url: "https://nexus6.kydom.net/repository/FrameworkA/LBFraudSDKiOS/LBFraudSDKiOS.xcframework.zip",
+                     checksum: "528c65775b1b668b08f225a481babeae2fa121feee710861fe8dc3de5cca0997")
     ]
 )
